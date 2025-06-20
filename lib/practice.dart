dec62429c340b342333300080e0846b3d7d9ca68
MD shahnawaz <mdshahnawazm474>
2025-06-21 02:43:33 +0530

    Initial commit

---
+++ lib/practice.dart
@@ -0,0 +1,18 @@
+
+void main(){
+  // String name = "shahnawaz";
+  // // print(name);
+  // var subject = "rohit ";
+  // subject = "3";
+  // // print(subject);
+
+var  myC=myClass();
+myC.printNames();
+
+}
+
+class myClass {
+  void printNames (){
+    print("ahaan khan");
+  }
+}
